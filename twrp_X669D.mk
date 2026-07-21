c65b00baf38690bdb33ad6e2501219beb5c5567d
Nyan <fgfy9023@gmail.com>
2026-07-21 07:05:49 +0000

    Update and rename twrp_X669.mk to twrp_X669D.mk

--- twrp_X669D.mk
+++ twrp_X669D.mk
@@ -4,34 +4,38 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-DEVICE_PATH := device/infinix/X669
+DEVICE_PATH := device/infinix/X669D
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from infinix-X669 device
-$(call inherit-product, device/infinix/X669/device.mk)
+# Inherit from infinix-X669D device
+$(call inherit-product, device/infinix/X669D/device.mk)
 
 
 
 
-PRODUCT_DEVICE := X669
-PRODUCT_NAME := twrp_X669
+PRODUCT_DEVICE := X669D
+PRODUCT_NAME := twrp_X669D
 PRODUCT_BRAND := infinix
-PRODUCT_MODEL := infinix X669
+PRODUCT_MODEL := infinix X669D
 PRODUCT_MANUFACTURER := infinix
-PRODUCT_RELEASE_NAME := infinix infinix X669
+PRODUCT_RELEASE_NAME := infinix infinix X669D
 
 
 
 
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="X669-user 12 SP1A.210812.016 490 release-keys"
+    PRIVATE_BUILD_DESC="X669D-user 12 SP1A.210812.016 63 release-keys"
 
-BUILD_FINGERPRINT := infinix/X669-GL/infinix-X669:12/SP1A.210812.016/GL-20231025V481:user/release-keys
+BUILD_FINGERPRINT := Infinix/X669D-GL/Infinix-X669D:12/SP1A.210812.016/GL-20240606V416:user/release-keys
